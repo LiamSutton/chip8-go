b617a6387143ff1c8efe5e9e7014cbc314910dd9
Liam <liamsutton1997@outlook.com>
2020-02-05 13:41:46 +0000

    Added logging to allow easier debugging

--- vm/opcodes.go
+++ vm/opcodes.go
@@ -16,49 +16,47 @@ func (cpu *CPU) opcode0x0000() {
 
 func (cpu *CPU) opcode0x1000() {
 	// Jump to address NNN
-	fmt.Println("0x1000: Jumping to address NNN")
+	fmt.Printf("0x1000: Jumping to address NNN: 0x%X\n", cpu.opcode&0x0FFF)
 	cpu.pc = cpu.opcode & 0x0FFF
 }
 
 func (cpu *CPU) opcode0x3000() {
 	// skip next instruction if Vx == NN
 	if cpu.v[(cpu.opcode&0x0F00)>>8] == uint8(cpu.opcode&0x00FF) {
-		fmt.Println("0x3000: Vx == NN, skipping next instruction")
+		fmt.Printf("0x3000: 0x%X = 0x%X, skipping next instruction\n", (cpu.opcode&0x0F00)>>8, uint8(cpu.opcode&0x00FF))
 		cpu.pc += 4
 	} else {
-		fmt.Println("0x3000: Vx != NN, not skipping next instruction")
+		fmt.Printf("0x3000: 0x%X != 0x%X, not skipping next instruction\n", (cpu.opcode&0x0F00)>>8, uint8(cpu.opcode&0x00FF))
 		cpu.pc += 2
 	}
 }
 
 func (cpu *CPU) opcode0x6000() {
 	// Put the value KK into the register Vx
-	fmt.Println("0x6000: Putting the value KK into register Vx")
+	fmt.Printf("0x6000: Putting the value KK: 0x%X  into register Vx: 0x%X\n", uint8(cpu.opcode&0x00FF), (cpu.opcode&0x0F00)>>8)
 	cpu.v[(cpu.opcode&0x0F00)>>8] = uint8(cpu.opcode & 0x00FF)
 	cpu.pc += 2
 }
 
 func (cpu *CPU) opcode0x7000() {
 	// Adds NN to Vx (carry flag unchanged)
-	fmt.Println("0x7000: Adding NN to Vx (carry flag unchanged")
+	fmt.Printf("0x7000: Adding NN: 0x%X to Vx: 0x%X (carry flag unchanged)\n", cpu.opcode&0x00FF, cpu.opcode&0x0F00>>8)
 	cpu.v[(cpu.opcode&0x0F00)>>8] += uint8(cpu.opcode & 0x00FF)
 	cpu.pc += 2
 }
 
 func (cpu *CPU) opcode0xA000() {
 	// Set I register -> NNNN
-	fmt.Println("0xA000: Setting I -> NNN")
+	fmt.Printf("0xA000: Setting I = 0x%X\n", cpu.opcode&0x0FFF)
 	cpu.i = cpu.opcode & 0x0FFF
 	cpu.pc += 2
 }
 
 func (cpu *CPU) opcode0xD000() {
-	// Draws sprite (dummy method for now)
-
 	x := uint16(cpu.v[(cpu.opcode&0x0F00)>>8])
 	y := uint16(cpu.v[(cpu.opcode&0x00F0)>>4])
 	height := cpu.opcode & 0x000F
-	fmt.Printf("0xD000: Drawing a sprite at X: %d, Y: %d with a height of %d", x, y, height)
+	fmt.Printf("0xD000: Drawing a sprite at X: 0x%X, Y: 0x%X with a height of 0x%X\n", x, y, height)
 	cpu.v[0xF] = 0
 
 	for yline := uint16(0); yline < height; yline++ {
