a204f3a9eb62fc02c841fab3557f72345cde335d
Liam <liamsutton1997@outlook.com>
2020-02-05 12:18:51 +0000

    Began to implement display using Pixel

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"github.com/LiamSutton/chip8-go/vm"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
 
 var roms = []string{"roms/Chip8 Picture.ch8"}
@@ -10,11 +14,41 @@ func main() {
 	cpu := vm.NewCPU()
 	cpu.ResetCPU()
 
-	rom := vm.ReadROM(roms[0])
+	// rom := vm.ReadROM(roms[0])
 
-	cpu.LoadROM(rom)
+	// cpu.LoadROM(rom)
 
-	for i := 0; i < 100; i++ {
-		cpu.EmulateCycle()
+	// for i := 0; i < 100; i++ {
+	// 	cpu.EmulateCycle()
+	// }
+	pixelgl.Run(run)
+}
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Chip-8 Go",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	imd := imdraw.New(nil)
+
+	imd.Color = pixel.RGB(1, 0, 0)
+	imd.Push(pixel.V(200, 100))
+	imd.Color = pixel.RGB(0, 1, 0)
+	imd.Push(pixel.V(800, 100))
+	imd.Color = pixel.RGB(0, 0, 1)
+	imd.Push(pixel.V(500, 700))
+	imd.Polygon(0)
+
+	for !win.Closed() {
+		win.Clear(colornames.Black)
+		imd.Draw(win)
+		win.Update()
 	}
 }
